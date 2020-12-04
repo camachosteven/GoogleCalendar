6ae987de9e0c520789830480e338989b0a1435f6
camachosteven <heatrcoks63134@gmail.com>
2020-12-04 11:13:04 -0500

    designed basic calendar structure

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const env = require('dotenv');
 
+env.config();
 const app = express();
 
+if (process.env.NODE_ENV === 'local') {
+    const livereload = require('livereload');
+    const connectLiveReload = require('connect-livereload');
+    app.use(connectLiveReload());
+    // livereload for development
+    const reloadServer = livereload.createServer();
+    reloadServer.watch(path.join(__dirname, 'MVC', 'Views'));
+    reloadServer.server.once('connection', () => {
+        setTimeout(() => {
+            reloadServer.refresh('/');
+        }, 100);
+    });
+}
+
 app.use(bodyParser.urlencoded({ extended: false }));
-express.static(path.join(__dirname, 'MVC', 'views', 'static'));
+app.use(express.static(path.join(__dirname, 'MVC', 'views', 'static')));
 
 app.set('views', 'MVC/views');
 app.set('view engine', 'ejs');
@@ -15,4 +31,5 @@ const mainRoutes = require('./routes/main');
 app.use(mainRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT);
+const HOST = process.env.HOST || '127.0.0.1';
+app.listen(PORT, HOST);
